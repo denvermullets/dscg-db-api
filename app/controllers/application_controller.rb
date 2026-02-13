f0f9fc67aa616d62f149a39ed402e503eb31d114
denvermullets <devmullets@gmail.com>
2026-02-13 18:44:51 -0500

    fix(routes): tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include Pagy::Method
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { error: e.message }, status: :not_found
+  end
 end
